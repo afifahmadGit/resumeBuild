699c46bbb2948687a9c3c83b054be76528474e9c
Afifahmad <afifahmadjaiinfo@gmail.com>
2023-02-12 19:45:02 +0530

    added material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
+import { ResumeComponent } from './layouts/resume/resume.component';
+import { ResumeDetailsComponent } from './resumeBuild/resume-details/resume-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path:"",redirectTo:'home',pathMatch:'full'},
+  {
+    path:'home',component:HomeLayoutComponent
+  },
+  {path:'resume-build',component:ResumeDetailsComponent,
+    children:[{path:'layouts',component:ResumeComponent}]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatIconModule} from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
+import { ResumeDetailsComponent } from './resumeBuild/resume-details/resume-details.component';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ResumeComponent } from './layouts/resume/resume.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeLayoutComponent,
+    ResumeDetailsComponent,
+    ResumeComponent,
+    SidenavComponent,
+    DashboardComponent
   ],
   imports: [
+    FormsModule,ReactiveFormsModule,MatSidenavModule,MatButtonModule,
+    MatIconModule,
+    DragDropModule,
     BrowserModule,
     AppRoutingModule
   ],
